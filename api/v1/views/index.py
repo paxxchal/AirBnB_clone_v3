c5a9f139790fb232f831e0fbeb3fadea196d1d5d
paxxchal <paschalgabrielacj8@gmail.com>
2024-05-28 06:39:58 +0100

    started API

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Index module for API """
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route("/status", methods=["GET"])
+def status():
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
