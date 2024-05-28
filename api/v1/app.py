c6f92db78524bb53ad804545fff5739be6a01c80
Paschal <59150676+paxxchal@users.noreply.github.com>
2024-05-28 07:32:51 +0100

    Fixed a bug

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
-""" API entry point """
-
-from flask import Flask
+"""
+Start a Flask web application.
+"""
+from flask import Flask, Blueprint, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
-import os
-
+from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close the storage on teardown"""
+def remove_session(response_or_exc):
+    """ Remove the current SQLAlchemy session """
     storage.close()
 
 
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", "5000"))
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(404)
+def not_found(self):
+    """Handle 404 error"""
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == '__main__':
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
+    if not HBNB_API_HOST:
+        HBNB_API_HOST = '0.0.0.0'
+    if not HBNB_API_PORT:
+        HBNB_API_PORT = 5000
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True, debug=True)
