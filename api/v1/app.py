a8d7d2f49933a51a942d3e3e176099482ec3ef3e
Paschal <59150676+paxxchal@users.noreply.github.com>
2024-05-28 08:59:23 +0100

    fixed errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python3
 """
-Start a Flask web application.
+0x05. AirBnB clone - RESTful API
 """
-from flask import Flask, Blueprint, jsonify
-from models import storage
-from os import getenv
-from api.v1.views import app_views
+
+from flask import Flask, jsonify
 from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+from os import environ
+
+#  Get host and port from environ if defined
+if environ.get('HBNB_API_HOST') is None:
+    HBNB_API_HOST = "0.0.0.0"
+else:
+    HBNB_API_HOST = environ.get('HBNB_API_HOST')
+if environ.get('HBNB_API_PORT') is None:
+    HBNB_API_PORT = 5000
+else:
+    HBNB_API_PORT = environ.get('HBNB_API_PORT')
+
+
 app = Flask(__name__)
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def remove_session(response_or_exc):
-    """ Remove the current SQLAlchemy session """
+def close_storage(exception=None):
+    """Close any active SQLAlchemy sessions"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(self):
-    """Handle 404 error"""
-    return jsonify({"error": "Not found"}), 404
+def not_found(error):
+    """ Error 404 """
+    return jsonify({'error': 'Not found'}), 404
+
 
 if __name__ == '__main__':
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
-    if not HBNB_API_HOST:
-        HBNB_API_HOST = '0.0.0.0'
-    if not HBNB_API_PORT:
-        HBNB_API_PORT = 5000
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True, debug=True)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
